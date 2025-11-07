5624759ffe28e0d62382bae1966c1cd9cd192c0d
Othallia <vianiputriocta6@gmail.com>
2025-11-07 16:01:16 +0700

    membuat composable tampildata

--- app/src/main/java/com/example/questnavigasiui_032/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_032/view/TampilData.kt
@@ -1,9 +1,17 @@
 package com.example.questnavigasiui_032.view
 
+import android.R.attr.id
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick: () -> Unit,
-)
+){
+    val items = listOf(
+        Pair(first = stringResource(id R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(id R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(id R.string.alamat), second = "Yogyakarta"),
+    )
+}
