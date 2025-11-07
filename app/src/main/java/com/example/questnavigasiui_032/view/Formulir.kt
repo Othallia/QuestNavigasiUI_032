9617dba3e25eb4d597fc7dda27cbf3667dc6b16b
Othallia <vianiputriocta6@gmail.com>
2025-11-07 15:09:57 +0700

    memperbaiki typo dan merapikan code

--- app/src/main/java/com/example/questnavigasiui_032/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_032/view/Formulir.kt
@@ -1,22 +1,47 @@
 package com.example.questnavigasiui_032.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposableTarget
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasiui_032.R
 
-@OptIn(...markerClass = ExperimentalMaterial3Api::clas)
+@OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
-    jenisK:List<String> = ListOf("Laki-laki", "Perempuan"),
-    onSubmitBtnClick : () -. Unit
-){
-    Scaffold (modifier=Modifier,
-        topBar ={
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    onSubmitBtnClick: () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
             TopAppBar(
-                title = {Text(text = stringResource(id=R.string.home), color = Color.White)},
+                title = { Text(text = stringResource(id = R.string.home), color = Color.White) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = colorResource(id = R.color.teal_700))
-            )}
-    ){ isiRuang ->
-        Column(modifier = Modifier.padding(paddingValues = isiRuang))
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = "",
+            )
+        }
     }
-})
+}
