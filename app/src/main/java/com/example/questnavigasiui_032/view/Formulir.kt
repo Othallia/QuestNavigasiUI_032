35ac3dae47a0c411728e892cc641cfcf153bec44
Othallia <vianiputriocta6@gmail.com>
2025-11-07 15:13:21 +0700

    menambahkan onvaluechange agar tidak bisa diisi

--- app/src/main/java/com/example/questnavigasiui_032/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_032/view/Formulir.kt
@@ -3,6 +3,7 @@ package com.example.questnavigasiui_032.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -15,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.questnavigasiui_032.R
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
@@ -41,6 +43,12 @@ fun FormIsian(
         ) {
             OutlinedTextField(
                 value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
             )
         }
     }
