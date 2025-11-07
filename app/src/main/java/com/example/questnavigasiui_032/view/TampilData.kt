7b7955e4429ea7153ea50de6aa24406b1e0767fe
Othallia <vianiputriocta6@gmail.com>
2025-11-07 15:57:55 +0700

    membuat composable tampildata

--- app/src/main/java/com/example/questnavigasiui_032/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_032/view/TampilData.kt
@@ -1,2 +1,9 @@
 package com.example.questnavigasiui_032.view
 
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick: () -> Unit,
+)
